7cee60f4855ca750b0890a0f586201afe22d2da2
hamza <hamzakayali10@gmail.com>
2020-08-16 23:03:40 +0300

    Son ders

--- src/git_project_sonders/SonDers.java
+++ src/git_project_sonders/SonDers.java
@@ -3,7 +3,7 @@ package git_project_sonders;
 public class SonDers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("21:55 - master branch");
 
 	}
 
