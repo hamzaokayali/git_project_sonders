af66a911253f996e305ff278cbb3dfceaaa0faa0
hamza <hamzakayali10@gmail.com>
2020-08-16 23:18:39 +0300

    feature commit

--- src/git_project_sonders/SonDers.java
+++ src/git_project_sonders/SonDers.java
@@ -5,6 +5,7 @@ public class SonDers {
 	public static void main(String[] args) {
 		System.out.println("21:55 - master branch");
 		System.out.println("22:06 - feature branch");
+		System.out.println("22:17 - feature branch");
 
 	}
 
